2d20c2c60d0b3af6fa98812ca4f796b3efb7d8e5
Borislav Stoyanov <bostoy@protonmail.com>
2022-04-02 23:40:56 +0200

    Remove unused app.service file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [AuthModule],
-  providers: [AppService],
 })
 export class AppModule {}
