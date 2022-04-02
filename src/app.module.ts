3366d4713489a6fe985f62cdad354dbf3eb5a6c2
Borislav Stoyanov <bostoy@protonmail.com>
2022-04-02 23:43:50 +0200

    generate bookmark module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, BookmarkModule],
 })
 export class AppModule {}
