5ce4e83e637d7d18acf652804f54f938039d60ef
Borislav Stoyanov <bostoy@protonmail.com>
2022-04-02 23:31:11 +0200

    Refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+(async () => {
+  const PORT = process.env.PORT || 8000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+  await app.listen(PORT, () => {
+    console.info("App running on port: ", PORT);
+  });
+})();
