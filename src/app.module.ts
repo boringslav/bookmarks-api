dafdfa2916019d21d4a231e5e943ef3f870da0a9
Borislav Stoyanov <bostoy@protonmail.com>
2022-04-03 00:50:42 +0200

    Generate repository module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { UserModule } from './user/user.module';
+import { RepositoryModule } from './repository/repository.module';
 
 @Module({
-  imports: [AuthModule, BookmarkModule, UserModule],
+  imports: [AuthModule, BookmarkModule, UserModule, RepositoryModule],
 })
 export class AppModule {}
