b23db2c6a2b75e2aa487eafe26b520f2194c29c1
Borislav Stoyanov <bostoy@protonmail.com>
2022-04-03 18:42:13 +0200

    Create a global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 (async () => {
   const PORT = process.env.PORT || 8000;
 
   const app = await NestFactory.create(AppModule);
+
+  /**
+   * Global validation pipe
+   * @property whitelist - when true it strips the properties from the request
+   *  that are not used in the application
+   */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
   await app.listen(PORT, () => {
-    console.info("App running on port: ", PORT);
+    console.info('App running on port: ', PORT);
   });
 })();
